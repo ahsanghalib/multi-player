a9f9d72e383f6919e29bf69ad118eb3ecbae61bb
ag <me@ag.com>
2022-12-13 01:08:57 +0500

    0.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
 import { DRMEnums, EventsEnum, TextTrackLabels } from "./lib/types";
 import { multiPlayer } from "./lib/MultiPlayer";
-(window as any).multiPlayer = multiPlayer;
-export default { multiPlayer };
+
+const exp = {
+  getPlayerState: multiPlayer.getPlayerState,
+  getMediaElement: multiPlayer.getMediaElement,
+  detachMediaElement: multiPlayer.detachMediaElement,
+  setSource: multiPlayer.setSource,
+  getCurrentConfig: multiPlayer.getCurrentConfig,
+  updateConfig: multiPlayer.updateConfig,
+  removeAllListeners: multiPlayer.removeAllListeners,
+  on: multiPlayer.on,
+  formatTime: multiPlayer.formatTime,
+  isFullScreen: multiPlayer.isFullScreen,
+  toggleFullScreen: multiPlayer.toggleFullScreen,
+  togglePip: multiPlayer.togglePip,
+  seekTime: multiPlayer.seekTime,
+  toggleForwardRewind: multiPlayer.toggleForwardRewind,
+  toggleMuteUnMute: multiPlayer.toggleMuteUnMute,
+  togglePlayPause: multiPlayer.togglePlayPause,
+  retry: multiPlayer.retry,
+  setTextTrack: multiPlayer.setTextTrack,
+  removePlayer: multiPlayer.removePlayer,
+  DRMEnums,
+  EventsEnum,
+  TextTrackLabels,
+};
+
+(window as any).multiPlayer = exp;
+
+export default { exp };
