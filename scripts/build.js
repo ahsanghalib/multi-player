9d113703228d9a155643a5d24bcec2c0ebbf6ff0
ahsanghalib <ahsanghalib@gmail.com>
2024-03-18 23:10:04 +0500

    build script update

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,30 @@
 #!/usr/bin/env node
-import * as esbuild from "esbuild";
-import pkg from "../package.json" assert { type: "json" };
-import { sassPlugin } from "esbuild-sass-plugin";
 
-esbuild
-  .build({
-    plugins: [sassPlugin()],
-    logLevel: "info",
-    entryPoints: ["src/index.ts"],
-    bundle: true,
-    minify: true,
-    splitting: true,
-    outdir: "dist",
-    ignoreAnnotations: true,
-    legalComments: "none",
-    target: ["esnext"],
-    format: "esm",
-    banner: {
-      js: `/* eslint-disable */\n/** ${pkg.name}-${pkg.version} */`,
-    },
-  })
-  .catch(() => process.exit(1));
+import { build } from 'esbuild';
+import pkg from '../package.json' assert { type: 'json' };
+import { sassPlugin } from 'esbuild-sass-plugin';
+import npmDts from 'npm-dts';
+
+const { Generator } = npmDts;
+
+new Generator({
+  entry: 'src/index.ts',
+  output: 'dist/index.d.ts',
+}).generate();
+
+build({
+  plugins: [sassPlugin()],
+  logLevel: 'info',
+  entryPoints: ['src/index.ts', 'src/player.scss'],
+  bundle: true,
+  minify: true,
+  splitting: true,
+  outdir: 'dist',
+  ignoreAnnotations: true,
+  legalComments: 'none',
+  target: ['esnext'],
+  format: 'esm',
+  banner: {
+    js: `/* eslint-disable */\n/** ${pkg.name}-${pkg.version} */`,
+  },
+}).catch(() => process.exit(1));
