8b623463f4b65f596941c2f796969150a9e9b7ca
ahsanghalib <ahsanghalib@gmail.com>
2024-03-18 19:59:58 +0500

    hls player integration

---
+++ src/hls.test.ts
@@ -0,0 +1,178 @@
+import Hls from "hls.js/dist/hls.min";
+import { HlsPlayer } from "./hls";
+import { ISource } from "./types";
+import { Utils } from "./utils";
+
+const addEventsMock = vi.fn();
+const isSupportedMock = vi.fn();
+const attachMediaMock = vi.fn();
+const playerStartLoadMock = vi.fn();
+const plyaerStopLoadMock = vi.fn();
+const playerDetachMediaMock = vi.fn();
+const playerDestroyMock = vi.fn();
+const playerONMock = vi.fn();
+const playerRemoveAllListenersMock = vi.fn();
+const toggleWrappersMock = vi.fn();
+const fatelErrorRetryMock = vi.fn();
+
+describe("hls player", () => {
+  test("init - not supported", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+
+    const originalAddEvents = hlsPlayer.addEvents;
+    hlsPlayer.addEvents = addEventsMock;
+
+    const originalHlsIsSupported = Hls.isSupported;
+    Hls.isSupported = isSupportedMock.mockImplementation(() => false);
+
+    hlsPlayer.init({} as any, {} as any, false).catch(() => {});
+
+    expect(console.error).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith("HLS not supported.");
+    expect(addEventsMock).not.toHaveBeenCalled();
+    expect(hlsPlayer.player).toBeNull();
+
+    hlsPlayer.addEvents = originalAddEvents;
+    Hls.isSupported = originalHlsIsSupported;
+  });
+
+  test("init - supported", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+
+    const originalAddEvents = hlsPlayer.addEvents;
+    hlsPlayer.addEvents = addEventsMock;
+
+    const originalHlsIsSupported = Hls.isSupported;
+    Hls.isSupported = isSupportedMock.mockImplementation(() => true);
+
+    hlsPlayer
+      .init({} as any, { url: "https://source.url" } as ISource, false)
+      .catch(() => {});
+
+    expect(console.error).not.toHaveBeenCalled();
+    expect(hlsPlayer.player).not.toBeNull();
+    expect(addEventsMock).toHaveBeenCalled();
+
+    hlsPlayer.addEvents = originalAddEvents;
+    Hls.isSupported = originalHlsIsSupported;
+  });
+
+  test("init - supported", async () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+
+    const originalHlsIsSupported = Hls.isSupported;
+    Hls.isSupported = isSupportedMock.mockImplementation(() => true);
+
+    hlsPlayer.player = new Hls({} as any);
+
+    hlsPlayer.player.attachMedia = attachMediaMock;
+
+    hlsPlayer
+      .init({} as any, { url: "https://source.url" } as ISource, false)
+      .catch(() => {});
+
+    expect(console.error).not.toHaveBeenCalled();
+    expect(hlsPlayer.player).not.toBeNull();
+    expect(attachMediaMock).toHaveBeenCalledTimes(1);
+
+    Hls.isSupported = originalHlsIsSupported;
+    hlsPlayer.player = null;
+  });
+
+  test("destroy", async () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+    hlsPlayer.player = new Hls({} as any);
+
+    hlsPlayer.player.stopLoad = plyaerStopLoadMock;
+    hlsPlayer.player.detachMedia = playerDetachMediaMock;
+    hlsPlayer.player.destroy = playerDestroyMock;
+
+    hlsPlayer.destroy().catch(() => {});
+
+    expect(plyaerStopLoadMock).toHaveBeenCalledTimes(1);
+    expect(playerDetachMediaMock).toHaveBeenCalledTimes(1);
+    expect(playerDestroyMock).toHaveBeenCalledTimes(1);
+    expect(hlsPlayer.player).toBeNull();
+
+    hlsPlayer.player = null;
+  });
+
+  test("startLoad", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+    hlsPlayer.isHlsStopped = true;
+    hlsPlayer.player = new Hls({} as any);
+
+    hlsPlayer.player.startLoad = playerStartLoadMock;
+
+    hlsPlayer.startLoad();
+
+    expect(playerStartLoadMock).toHaveBeenCalledTimes(1);
+    expect(hlsPlayer.isHlsStopped).toBeFalsy();
+
+    hlsPlayer.isHlsStopped = true;
+    hlsPlayer.startLoad(200);
+
+    expect(playerStartLoadMock).toHaveBeenCalledTimes(2);
+    expect(playerStartLoadMock).toHaveBeenCalledWith(200);
+    expect(hlsPlayer.isHlsStopped).toBeFalsy();
+  });
+
+  test("stopLoad", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+    hlsPlayer.isHlsStopped = false;
+    hlsPlayer.player = new Hls({} as any);
+
+    hlsPlayer.player.stopLoad = plyaerStopLoadMock;
+
+    hlsPlayer.stopLoad();
+
+    expect(plyaerStopLoadMock).toHaveBeenCalledTimes(1);
+    expect(hlsPlayer.isHlsStopped).toBeTruthy();
+  });
+
+  test("addEvents", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+    hlsPlayer.player = new Hls({} as any);
+
+    hlsPlayer.player.on = playerONMock;
+
+    hlsPlayer.addEvents();
+
+    expect(playerONMock).toHaveBeenCalledTimes(1);
+    expect(playerONMock).toHaveBeenCalledWith(
+      Hls.Events.ERROR,
+      expect.any(Function)
+    );
+  });
+
+  test("removeEvents", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+    hlsPlayer.player = new Hls({} as any);
+
+    hlsPlayer.player.removeAllListeners = playerRemoveAllListenersMock;
+
+    hlsPlayer.removeEvents();
+
+    expect(playerRemoveAllListenersMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("errorEvent - not fatal", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+
+    Utils.toggleWrappers = toggleWrappersMock;
+
+    hlsPlayer.errorEvent({} as any, { details: "bufferStalledError" });
+
+    expect(toggleWrappersMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("errorEvent - fatal", () => {
+    const hlsPlayer = new HlsPlayer({} as any);
+
+    Utils.fatelErrorRetry = fatelErrorRetryMock;
+
+    hlsPlayer.errorEvent({} as any, { fatal: true });
+
+    expect(fatelErrorRetryMock).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/hls.ts
@@ -0,0 +1,103 @@
+import Hls from "hls.js/dist/hls.min";
+import { ISource } from "./types";
+import { UI } from "./ui";
+import { Utils } from "./utils";
+
+export class HlsPlayer {
+  private ui: UI;
+  player: typeof Hls | null = null;
+  isHlsStopped = false;
+
+  constructor(ui: UI) {
+    this.ui = ui;
+  }
+
+  init = async (video: HTMLVideoElement, source: ISource, debug: boolean) => {
+    if (!Hls.isSupported()) {
+      console.error("HLS not supported.");
+      return Promise.reject();
+    }
+
+    Utils.urlCheck(source);
+
+    if (!this.player) {
+      this.player = new Hls({
+        ...Hls.DefaultConfig,
+        debug,
+        startPosition: source.startTime ?? -1,
+        liveDurationInfinity: true,
+        enableWorker: true,
+        // lowLatencyMode: true,
+        // backBufferLength: 30,
+        maxBufferLength: 30,
+        // maxMaxBufferLength: 300,
+        maxBufferSize: 3e7,
+        // maxLoadingDelay: 1,
+        // maxBufferLength: 10,
+        // maxMaxBufferLength: 300,
+        // maxBufferSize: 30 * 1000 * 1000,
+        // liveSyncDurationCount: 1,
+        // progressive: true,
+      });
+    }
+
+    this.player.attachMedia(video);
+
+    /* c8 ignore start */
+    this.player.on(Hls.Events.MEDIA_ATTACHED, () => {
+      this.player.loadSource(source.url ?? "");
+    });
+    /* c8 ignore stop */
+
+    this.addEvents();
+
+    return Promise.resolve();
+  };
+
+  destroy = async () => {
+    if (this.player) {
+      this.player.stopLoad();
+      this.player.detachMedia();
+      this.player.destroy();
+      this.player = null;
+    }
+    return Promise.resolve();
+  };
+
+  startLoad = (startPosition?: number) => {
+    if (this.player && this.isHlsStopped) {
+      this.player.startLoad(startPosition);
+      this.isHlsStopped = false;
+    }
+  };
+
+  stopLoad = () => {
+    if (this.player) {
+      this.player.stopLoad();
+      this.isHlsStopped = true;
+    }
+  };
+
+  addEvents = () => {
+    if (this.player) {
+      this.player.on(Hls.Events.ERROR, this.errorEvent.bind(this));
+    }
+  };
+
+  removeEvents = () => {
+    if (this.player) {
+      this.player.removeAllListeners();
+    }
+  };
+
+  errorEvent = (e: any, d: any) => {
+    console.log("hls-error", e, d);
+    if (d?.details === "bufferStalledError") {
+      Utils.toggleWrappers({ ui: this.ui, loading: true });
+    }
+
+    if (d?.fatal) {
+      Utils.fatelErrorRetry(this.ui);
+    }
+  };
+}
