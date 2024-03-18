7f7071f7a98b72db39e9f0659227f030632a2c24
ahsanghalib <ahsanghalib@gmail.com>
2024-03-19 00:07:39 +0500

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 import { playerInstance } from './player';
 import { DRMEnums } from './types';
 
-export { playerInstance as default, DRMEnums };
+const player = {
+  instance: playerInstance,
+  DRMEnums: DRMEnums,
+  init: playerInstance.init,
+  getVideoElement: playerInstance.ui.getVideoElement,
+  getPlayerState: playerInstance.getPlayerState,
+  setPlayerState: playerInstance.setPlayerState,
+  unmount: playerInstance.unmount,
+  removePlayer: playerInstance.removePlayer,
+  onTogglePlayPause: playerInstance.onTogglePlayPause,
+  onToggleMuteUnMute: playerInstance.onToggleMuteUnMute,
+  onToggleForwardRewind: playerInstance.onToggleForwardRewind,
+  onSeekTime: playerInstance.onSeekTime,
+  onTogglePip: playerInstance.onTogglePip,
+  onToggleFullScreen: playerInstance.onToggleFullScreen,
+  onEndedReplay: playerInstance.onEndedReplay,
+  setCastingSource: playerInstance.castSender.sendSourceInfo,
+  setCastingMediaInfo: playerInstance.castSender.sendMediaInfo,
+  stopCasting: playerInstance.castSender.stopCasting,
+};
+
+export { player as default, DRMEnums };
