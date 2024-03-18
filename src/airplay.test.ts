8e51a0352198859cdf4f896144d43f31c645bf05
ahsanghalib <ahsanghalib@gmail.com>
2024-03-18 20:00:15 +0500

    airplay

---
+++ src/airplay.test.ts
@@ -0,0 +1,242 @@
+import { Utils } from "./utils";
+import { AirPlay } from "./airplay";
+
+class AirplayTestClass extends AirPlay {
+  constructor(ui: any) {
+    super(ui);
+  }
+
+  webkitPlaybackTargetAvailabilityChangedEventExtended = (
+    airplay: HTMLDivElement,
+    video: any,
+    event: any
+  ) => {
+    this.webkitPlaybackTargetAvailabilityChangedEvent(airplay, video)(event);
+  };
+
+  webkitCurrentPlaybackTargetIsWirelessChangedEventExtended = (
+    airplay: HTMLDivElement,
+    isLive: boolean,
+    volume: HTMLDivElement,
+    event: any
+  ) => {
+    this.webkitCurrentPlaybackTargetIsWirelessChangedEvent(
+      airplay,
+      isLive,
+      volume
+    )(event);
+  };
+}
+
+const isLiveMock = vi.fn();
+const getBrowserMock = vi.fn();
+const addEventListenerMock = vi.fn();
+const airplayDivOnClickMock = vi.fn();
+const airplayDivClassListRemoveMock = vi.fn();
+const airplayDivClassListAddMock = vi.fn();
+const webkitShowPlaybackTargetPickerMock = vi.fn();
+const toggleShowHideMock = vi.fn();
+const setPlayerStateMock = vi.fn();
+const reloadPlayerMock = vi.fn(() => ({ catch: vi.fn() }));
+
+describe("AirPlay", () => {
+  test("init - not supported.", () => {
+    const airplay = new AirplayTestClass({});
+
+    (window as any).WebKitPlaybackTargetAvailabilityEvent = false;
+
+    const originalGetBrowser = Utils.getBrowser;
+    const originalIsLive = Utils.isLive;
+
+    Utils.getBrowser = getBrowserMock.mockReturnValue("chrome");
+    Utils.isLive = isLiveMock.mockReturnValue(false);
+
+    airplay.init();
+
+    expect(isLiveMock).not.toHaveBeenCalled();
+
+    Utils.getBrowser = originalGetBrowser;
+    Utils.isLive = originalIsLive;
+  });
+
+  test("init - supported", () => {
+    const airplay = new AirplayTestClass({
+      videoElement: {
+        addEventListener: addEventListenerMock,
+      },
+      controlsRemotePlaybackButton: {},
+      controlsVolumeWrapper: {},
+    });
+
+    (window as any).WebKitPlaybackTargetAvailabilityEvent = true;
+
+    const originalGetBrowser = Utils.getBrowser;
+    const originalIsLive = Utils.isLive;
+
+    Utils.getBrowser = getBrowserMock.mockReturnValue("safari");
+    Utils.isLive = isLiveMock.mockReturnValue(false);
+
+    airplay.init();
+
+    expect(isLiveMock).toHaveBeenCalledTimes(1);
+    expect(addEventListenerMock).toHaveBeenCalledTimes(2);
+
+    Utils.getBrowser = originalGetBrowser;
+    Utils.isLive = originalIsLive;
+  });
+
+  test("webkitPlaybackTargetAvailabilityChangedEvent - available", () => {
+    const airplay = new AirplayTestClass({});
+
+    const airplayDiv = {
+      innerHTML: "",
+      onclick: airplayDivOnClickMock,
+      classList: {
+        remove: airplayDivClassListRemoveMock,
+      },
+    };
+
+    const videoMock = {
+      webkitShowPlaybackTargetPicker: webkitShowPlaybackTargetPickerMock,
+    };
+
+    const eventMock = {
+      availability: "available",
+    };
+
+    airplay.webkitPlaybackTargetAvailabilityChangedEventExtended(
+      airplayDiv as any,
+      videoMock,
+      eventMock
+    );
+
+    expect(airplayDivOnClickMock).toHaveBeenCalledTimes(0);
+    expect(airplayDivClassListRemoveMock).toHaveBeenCalledTimes(1);
+    expect(airplayDivClassListRemoveMock).toHaveBeenCalledWith("none");
+    expect(airplayDiv.innerHTML).toEqual(
+      Utils.Icons({ type: "airplay_enter" })
+    );
+
+    airplayDiv.onclick();
+    expect(webkitShowPlaybackTargetPickerMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("webkitPlaybackTargetAvailabilityChangedEvent - not available", () => {
+    const airplay = new AirplayTestClass({});
+
+    const airplayDiv = {
+      innerHTML: Utils.Icons({ type: "airplay_enter" }),
+      classList: {
+        add: airplayDivClassListAddMock,
+      },
+    };
+
+    const videoMock = {};
+
+    const eventMock = {
+      availability: "not-available",
+    };
+
+    airplay.webkitPlaybackTargetAvailabilityChangedEventExtended(
+      airplayDiv as any,
+      videoMock,
+      eventMock
+    );
+
+    expect(airplayDivClassListAddMock).toHaveBeenCalledTimes(1);
+    expect(airplayDivClassListAddMock).toHaveBeenCalledWith("none");
+    expect(airplayDiv.innerHTML).toEqual("");
+  });
+
+  test("webkitCurrentPlaybackTargetIsWirelessChangedEvent - connected", () => {
+    const airplay = new AirplayTestClass({
+      player: {
+        setPlayerState: setPlayerStateMock,
+        reloadPlayer: reloadPlayerMock,
+        playerState: {
+          isPlaying: true,
+        },
+      },
+    });
+
+    const airplayDiv = {
+      innerHTML: Utils.Icons({ type: "airplay_enter" }),
+    };
+
+    const volumeDiv = {};
+
+    const eventMock = {
+      target: {
+        remote: {
+          state: "connected",
+        },
+      },
+    };
+
+    const originalToggleShowHide = Utils.toggleShowHide;
+    Utils.toggleShowHide = toggleShowHideMock;
+
+    airplay.webkitCurrentPlaybackTargetIsWirelessChangedEventExtended(
+      airplayDiv as any,
+      true,
+      volumeDiv as any,
+      eventMock
+    );
+
+    expect(airplayDiv.innerHTML).toEqual(Utils.Icons({ type: "airplay_exit" }));
+    expect(toggleShowHideMock).toHaveBeenCalledTimes(1);
+    expect(toggleShowHideMock).toHaveBeenCalledWith(volumeDiv, "none");
+    expect(setPlayerStateMock).toHaveBeenCalledTimes(1);
+    expect(setPlayerStateMock).toHaveBeenCalledWith({ isAirplay: true });
+    expect(reloadPlayerMock).toHaveBeenCalledTimes(1);
+
+    Utils.toggleShowHide = originalToggleShowHide;
+  });
+
+  test("webkitCurrentPlaybackTargetIsWirelessChangedEvent - disconnected", () => {
+    const airplay = new AirplayTestClass({
+      player: {
+        setPlayerState: setPlayerStateMock,
+        reloadPlayer: reloadPlayerMock,
+        playerState: {
+          isPlaying: true,
+        },
+      },
+    });
+
+    const airplayDiv = {
+      innerHTML: Utils.Icons({ type: "airplay_exit" }),
+    };
+
+    const volumeDiv = {};
+
+    const eventMock = {
+      target: {
+        remote: {
+          state: "disconnected",
+        },
+      },
+    };
+
+    const originalToggleShowHide = Utils.toggleShowHide;
+    Utils.toggleShowHide = toggleShowHideMock;
+
+    airplay.webkitCurrentPlaybackTargetIsWirelessChangedEventExtended(
+      airplayDiv as any,
+      true,
+      volumeDiv as any,
+      eventMock
+    );
+
+    expect(airplayDiv.innerHTML).toEqual(
+      Utils.Icons({ type: "airplay_enter" })
+    );
+    expect(toggleShowHideMock).toHaveBeenCalledTimes(1);
+    expect(toggleShowHideMock).toHaveBeenCalledWith(volumeDiv, "flex");
+    expect(setPlayerStateMock).toHaveBeenCalledTimes(1);
+    expect(setPlayerStateMock).toHaveBeenCalledWith({ isAirplay: false });
+    expect(reloadPlayerMock).toHaveBeenCalledTimes(1);
+
+    Utils.toggleShowHide = originalToggleShowHide;
+  });
+});
---
+++ src/airplay.ts
@@ -0,0 +1,96 @@
+import { BrowsersEnum } from "./types";
+import { UI } from "./ui";
+import { Utils } from "./utils";
+
+export class AirPlay {
+  private ui: UI;
+
+  constructor(ui: UI) {
+    this.ui = ui;
+  }
+
+  protected webkitPlaybackTargetAvailabilityChangedEvent = (
+    airplay: HTMLDivElement,
+    video: any
+  ) => {
+    return (event: any) => {
+      switch (event?.availability) {
+        case "available":
+          airplay.innerHTML = Utils.Icons({
+            type: "airplay_enter",
+          });
+          airplay.classList.remove("none");
+          airplay.onclick = () => {
+            video.webkitShowPlaybackTargetPicker();
+          };
+          break;
+        case "not-available":
+        default:
+          airplay.innerHTML = "";
+          airplay.classList.add("none");
+          break;
+      }
+    };
+  };
+
+  protected webkitCurrentPlaybackTargetIsWirelessChangedEvent = (
+    airplay: HTMLDivElement,
+    isLive: boolean,
+    volume: HTMLDivElement
+  ) => {
+    return (event: any) => {
+      const state = event?.target?.remote?.state;
+      if (state === "connected") {
+        airplay.innerHTML = Utils.Icons({
+          type: "airplay_exit",
+        });
+        Utils.toggleShowHide(volume, "none");
+        this.ui.player.setPlayerState({ isAirplay: true });
+        if (this.ui.player.playerState.isPlaying && isLive) {
+          this.ui.player.reloadPlayer().catch(() => console.log());
+        }
+      } else if (state === "disconnected") {
+        airplay.innerHTML = Utils.Icons({
+          type: "airplay_enter",
+        });
+        Utils.toggleShowHide(volume, "flex");
+        this.ui.player.setPlayerState({ isAirplay: false });
+        if (this.ui.player.playerState.isPlaying && isLive) {
+          this.ui.player.reloadPlayer().catch(() => console.log());
+        }
+      }
+    };
+  };
+
+  init = () => {
+    if (
+      Utils.getBrowser() === BrowsersEnum.SAFARI &&
+      (window as any).WebKitPlaybackTargetAvailabilityEvent
+    ) {
+      const video = this.ui.videoElement;
+      const airplay = this.ui.controlsRemotePlaybackButton;
+      const volume = this.ui.controlsVolumeWrapper;
+      const isLive = Utils.isLive(this.ui);
+
+      const webkitPlaybackTargetAvailabilityChanged =
+        this.webkitPlaybackTargetAvailabilityChangedEvent(airplay, video);
+
+      const webkitCurrentPlaybackTargetIsWirelessChanged =
+        this.webkitCurrentPlaybackTargetIsWirelessChangedEvent(
+          airplay,
+          isLive,
+          volume
+        );
+
+      video.addEventListener(
+        "webkitplaybacktargetavailabilitychanged",
+        webkitPlaybackTargetAvailabilityChanged
+      );
+
+      video.addEventListener(
+        "webkitcurrentplaybacktargetiswirelesschanged",
+        webkitCurrentPlaybackTargetIsWirelessChanged
+      );
+    }
+  };
+}
