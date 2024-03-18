2085e2ff8887168b836fa4cec7f953ca777e3d1c
ahsanghalib <ahsanghalib@gmail.com>
2024-03-18 20:00:18 +0500

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-import { DRMEnums, EventsEnum, TextTrackLabels } from './lib/types';
-import { multiPlayer } from './lib/MultiPlayer';
+import { playerInstance } from "./player";
+import "./player.scss";
+import { DRMEnums } from "./types";
 
-const exp = {
-  getPlayerState: multiPlayer.getPlayerState,
-  setPlayerState: multiPlayer.setPlayerState,
-  getMediaElement: multiPlayer.getMediaElement,
-  detachMediaElement: multiPlayer.detachMediaElement,
-  setSource: multiPlayer.setSource,
-  getCurrentConfig: multiPlayer.getCurrentConfig,
-  updateConfig: multiPlayer.updateConfig,
-  removeAllListeners: multiPlayer.removeAllListeners,
-  on: multiPlayer.on,
-  formatTime: multiPlayer.formatTime,
-  isFullScreen: multiPlayer.isFullScreen,
-  toggleFullScreen: multiPlayer.toggleFullScreen,
-  togglePip: multiPlayer.togglePip,
-  seekTime: multiPlayer.seekTime,
-  toggleForwardRewind: multiPlayer.toggleForwardRewind,
-  toggleMuteUnMute: multiPlayer.toggleMuteUnMute,
-  togglePlayPause: multiPlayer.togglePlayPause,
-  retry: multiPlayer.retry,
-  setTextTrack: multiPlayer.setTextTrack,
-  removePlayer: multiPlayer.removePlayer,
-	isLive: multiPlayer.isLive,
-  DRMEnums,
-  EventsEnum,
-  TextTrackLabels,
-};
-
-(window as any).multiPlayer = exp;
-
-export default exp;
+export { playerInstance as default, DRMEnums };
